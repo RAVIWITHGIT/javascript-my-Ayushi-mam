fbfa5c3161f742b585b8dca5551b6afe3a8e0284
ravi lakshakar <ravilakshakar336@gmail.com>
2023-11-23 19:52:11 +0530

    For Of Loop

---
+++ tut7/app.js
@@ -0,0 +1,34 @@
+// ------------------------- for of loop ----------------------------------------
+// let arr = [1,2,3,4,5,6,7,8,9]
+// for(let i of arr){
+//  console.log(i+1)
+// }
+
+// a = []
+// b = []
+// function myfun(arr) {
+    
+//     for (let i of arr){
+//         if(i>50){
+//             a.push(i)
+//         }
+//         else{
+//             b.push(i)
+//         }
+
+// }
+    
+// }
+
+// let arr = [12,25,31,47,51,69,73,89,99,10]
+// myfun(arr)
+// console.log(a)
+// console.log(b)
+
+// ---------------------------- reverse --------------------------
+// let arr = [12,25,31,47,51,69,73,89,99,10]
+
+// for (let i = arr.length-1; i >= 0; i--) {
+//     console.log(arr[i])
+    
+// }
