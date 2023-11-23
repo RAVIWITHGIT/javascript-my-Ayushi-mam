771ef8d8010bdd2e46f61ed67efe1df06c013428
ravi lakshakar <ravilakshakar336@gmail.com>
2023-11-23 19:43:16 +0530

    data type

---
+++ tut2/index.js
@@ -0,0 +1,38 @@
+console.log('hello world')
+
+//----------------------------------- data type--------------------
+// two types of data type
+
+/* first - primitive data type 
+    nnssbbu
+    null,number,string,symbol boolean,bigint,undefined
+
+*/
+/* second -non primitive data type
+    obect:array,obect,date
+
+*/
+
+
+// const fun = ()=>{
+//     let a = 10
+//     let b= 20
+//     let c = a+b
+//     console.log(c)
+// }
+
+// fun()
+
+
+
+const add = (a,b)=>{
+    let c = a+b
+    console.log(c)
+}
+
+add(4,5)
+
+
+
+
+
