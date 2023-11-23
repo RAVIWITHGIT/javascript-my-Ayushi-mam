99ab719e58c5502b101bddea95c50104f0a46827
ravi lakshakar <ravilakshakar336@gmail.com>
2023-11-23 19:42:07 +0530

    first commit

---
+++ tut1/index.js
@@ -0,0 +1,67 @@
+console.log('hello world')
+
+
+
+// var a = 10
+// a = 20
+// console.log(a)
+
+// -redecleared var varaible
+// var a = 30
+// console.log(a)
+
+// ---- var is global scope
+// var a = 10
+// console.log(a)
+// {
+//     console.log('inner',a)
+//     var a = 50
+// }
+// console.log(a)
+
+
+
+// ------------------------------------------------------------------
+// let a = 10
+// a = 20
+// console.log(a)
+
+// -----------we can not  redecleared let varaible
+// let a = 40
+// console.log(a)
+
+// ---- let is not global scope
+// let a = 10
+// {
+//     a = 50
+// }
+// console.log(a)
+
+
+// {
+//    let a = 50
+//    console.log(a)
+// }
+// console.log(a)
+
+
+
+
+// -------------------------------------------------------------------
+
+// const a1 = 10
+
+// --we can not update const varabiel
+// a1 = 20
+// console.log(a1)
+
+// ---------we can not  redecleared const varaible
+// const a = 40
+// console.log(a)
+
+// ---- const is not global scope
+// {
+//     const a = 10
+// }
+// console.log(a)
+
