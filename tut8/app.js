2de8577087a610dee1bb276c10f1cd239703d962
ravi lakshakar <ravilakshakar336@gmail.com>
2023-11-23 19:53:26 +0530

    For In Loop

---
+++ tut8/app.js
@@ -0,0 +1,58 @@
+// --------------------- for in loop -------------------------------------------------
+// let myArray = [23,45,12,56,78,54,89]
+// for(let i in myArray){
+//     console.log(i)
+// }
+
+
+// -------- object
+let myobj = {
+    name:'ravi',
+    from:'jaipur',
+    pincod:303301
+}
+
+// -----we can not use for of loop in object
+// for(let i of myobj){
+//     console.log(i)
+// }
+
+// ----- 
+// for(let i in myobj){
+//     // console.log(i)
+//     console.log(myobj[i])
+// }
+
+
+// ----- we can not write this in object when access value
+// console.log(myobj[from])
+
+// ------ if we want use object key then we write in singal code and double code in key
+
+// console.log(myobj["from"])
+
+// -------------------------------while loop -------------------------------------------------
+// let i = 0
+// while(i<10){
+//     console.log(i)
+//     i++
+// }
+
+
+// --------------------------- do while loop ----------------------------------
+
+// if we want run code minimum one time using loop without check condition then use do while loop
+
+// let i = 0
+// do {
+//     console.log('one time run')
+//     i++
+    
+// } while (i>10);
+
+
+
+
+
+
+
---
+++ tut8/task.js
@@ -0,0 +1,46 @@
+// let even = []
+// let odd = []
+// let myArray = [2,3,4,66,23,90,34,55,79,81,24,34,51]
+// for (let i = 0; i < myArray.length; i++) {
+//     console.log(myArray[i])
+//     if(myArray[i]%2==0){
+//         even.push(myArray[i])
+//     }
+//     else{
+//         odd.push(myArray[i])
+//     }
+    
+// }
+
+// console.log(even)
+// console.log(odd)
+
+// -----------------------------------------------
+// let myArray = [2,3,4,5,6,5,4,5,6,7,8]
+// let i = 0
+// let total = 0
+// while (true) {
+//     if(i<myArray.length){
+//         total += myArray[i]
+
+//     }
+//     else{
+//         break
+//     }
+//     i++
+
+    
+// }
+// console.log(total)
+
+
+// -----------------------------------------------
+// let getValue = prompt('enter your name')
+// let userName = 'ravi'
+// do{
+//     let getValue = prompt('sorry again enter your name')
+//     if(getValue==userName){
+//         break
+//     }
+
+// }while(true)
