b19b2ed276663eec067b391030cfda4e1c24900e
ravi lakshakar <ravilakshakar336@gmail.com>
2023-11-23 19:50:02 +0530

    Object With Destructuring

---
+++ tut6/app.js
@@ -0,0 +1,49 @@
+console.log('hay ravi')
+// const myarray = [12,33,4,5,6]
+// const [a,b] = myarray
+// console.log(a)
+// console.log(b)
+
+// ------- spread operator
+// const [a,...b] = myarray
+// console.log(a)
+// console.log(b)
+
+// let myArray = ['ravi',true,23,45,['jaipur',34]]
+// console.log(myArray)
+// let [f,s,t,fo,fv] = myArray
+// console.log(f)
+// console.log(s)
+// console.log(t)
+// console.log(fo)
+// console.log(fv)
+
+// --------------------------- object ----------------------------
+const myobj = {
+    a:'ravi',
+    b:'jaipur',
+    c:'coding'
+
+}
+
+// console.log(obj)
+// console.log(obj.name)
+
+// -------- object destructuring -------
+
+
+// const {a,b,c} = myobj
+// console.log(a,b,c)
+
+
+
+// const {a:x,b,c} = myobj
+// console.log(x)
+
+// -------- spread operator in object
+
+// const {a,...b} = myobj
+// console.log(a,b)
+
+// const {a,...x} = myobj
+// console.log(a,x)
