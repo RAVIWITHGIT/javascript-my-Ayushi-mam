2aecbf896aec77e4e026646d1c2000f892a07ceb
ravi lakshakar <ravilakshakar336@gmail.com>
2023-11-23 19:45:55 +0530

    prime question

---
+++ tut4/app.js
@@ -0,0 +1,15 @@
+function isprime(num){
+    if(num==2 || num==3){
+        return true
+    }
+    else{
+        for (let i = 2; i < num; i++) {
+            if(num%i==0){
+                return false
+            }
+        }
+        return true
+    }
+}
+    
+console.log(isprime(93))
