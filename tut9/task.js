91fdeaca5bdae5981c92a9eca9f23b75306df011
ravi lakshakar <ravilakshakar336@gmail.com>
2023-11-27 10:52:29 +0530

    modify task.js

--- tut9/task.js
+++ tut9/task.js
@@ -1,24 +1,49 @@
-let myArray = [1, 2, 3,7, 2, 4, 3,7, 5,5,5, 6, 4,7, 7,7,5, 1]
-let myobj = {}
-for (let i = 0; i < myArray.length; i++) {
-    let element = myArray[i]
-    if(myobj[element]){
-        myobj[element]++
-    }else{
-        myobj[element] = 1
-    }
+// ---------------------------find maximum time element present
+// let myArray = [1, 2, 3,7, 2, 4, 3,7, 5,5,5, 6, 4,7, 7,7,5, 1]
+// let myobj = {}
+// for (let i = 0; i < myArray.length; i++) {
+//     let element = myArray[i]
+//     if(myobj[element]){
+//         myobj[element]++
+//     }else{
+//         myobj[element] = 1
+//     }
     
-}
-console.log(myobj)
+// }
+// console.log(myobj)
 
-let maxcount = 0
-let element;
-for(let i in myobj){
-    if(myobj[i]>maxcount){
-        maxcount = myobj[i]
-        element = i
-    }
+// let maxcount = 0
+// let element;
+// for(let i in myobj){
+//     if(myobj[i]>maxcount){
+//         maxcount = myobj[i]
+//         element = i
+//     }
 
-}
-console.log(element)
-console.log(maxcount)
+// }
+// console.log(element)
+// console.log(maxcount)
+
+// ------------------------------- swap arr1 value to arr2 and arr2 value to arr1
+// let arr1 = ['ravi','shiv','ram','shyam']
+// let arr2 = [1,2,3,4,5,6]
+
+// let narr = [...arr2]
+// arr2 = [...arr1]
+// arr1 = [...narr]
+
+// console.log(narr)
+// console.log(arr2)
+// console.log(arr1)
+
+
+// ----------------- get length after splice
+// let myarr = ['ravi','shiv','ram','shyam']
+// function afterrem(getindex){
+//     myarr.splice(getindex,1)
+//     console.log(myarr)
+//     console.log(myarr.length)
+
+
+// }
+// afterrem(1)
