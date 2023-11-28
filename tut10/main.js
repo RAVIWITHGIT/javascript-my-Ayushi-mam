1800c2fa925c900fe5d69755719da3cb9bde36c2
ravi lakshakar <ravilakshakar336@gmail.com>
2023-11-28 19:35:44 +0530

    update my file

---
+++ tut10/main.js
@@ -0,0 +1,67 @@
+// ---------------------- hosting------------------------------------------------------
+// console.log(a)
+// var a;
+
+// console.log(b)
+// var b = 10
+// console.log(b)
+
+
+// -----------------------------------
+
+// ------- hosting work only declartion function
+// myname()
+
+// function myname(){
+//     console.log('ravi lakshakar')
+// }
+
+// -- ------------------------ below  function is express function so not work hosting
+// myname()
+
+// const myname = function(){
+//     console.log('ravi lakshakar')
+// }
+
+
+// ---
+
+// myname()
+
+// let myname = function(){
+//     console.log('ravi lakshakar')
+// }
+
+// ----
+
+// myname()
+
+// var myname = function(){
+//     console.log('ravi lakshakar')
+// }
+
+
+// --------------------- callback function ---
+// function myfirst(a){
+//     console.log('hello')
+//     a()
+// }
+
+// function mysecond(){
+//     console.log('goodbye')
+// }
+
+// myfirst(mysecond)
+
+
+// function myfirst(a){
+//     console.log('hello')
+//     a()
+// }
+
+// function mysecond(){
+//     console.log('goodbye')
+// }
+
+// var b = mysecond
+// myfirst(b)
