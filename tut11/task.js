63983a15d3b523acade6e35fff4218bd3174166a
ravi lakshakar <ravilakshakar336@gmail.com>
2023-11-30 12:10:32 +0530

    update my code

---
+++ tut11/task.js
@@ -0,0 +1,43 @@
+let festival= 'march'
+
+switch(festival){
+  case 'january':
+    console.log('makar sankranti')
+    break;
+  case 'february':
+    console.log('vasant panchami')
+    break;
+  case 'march':
+    console.log('Holi')
+    break;
+  case 'april':
+    console.log('Ram Navami')
+    break;
+  case 'may':
+    console.log('Rabindranath Tagore Jayanti')
+    break;
+  case 'june':
+    console.log("father's day")
+    break;
+  case 'july':
+    console.log('Muharram')
+    break;
+  case 'august':
+    console.log('krishna janmashtami')
+    break;
+  case 'september':
+    console.log('Ganesh Chaturthi')
+    break;
+  case 'october':
+    console.log('Dusshera')
+    break;
+  case 'november':
+    console.log('Diwali')
+    break;
+  case 'december':
+    console.log('Christmas')
+    break;
+  default :
+    console.log('invalid month ')
+    
+}
