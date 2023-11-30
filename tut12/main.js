69b56d786c0ee99c639c80986d4f2f209e3fc8bd
ravi lakshakar <ravilakshakar336@gmail.com>
2023-11-30 16:21:13 +0530

    update code

---
+++ tut12/main.js
@@ -0,0 +1,72 @@
+// ---------------------------------- this keyword-----------------------
+// const person = {
+//     'name':'ravi',
+//     'roll':6,
+//     'xyz': function name() {
+//         console.log('hey ravi')
+        
+//     }
+// }
+
+// // let getfun = person['xyz']()
+// let getfun = person.xyz()
+
+
+// ------------------------
+// const person = {
+//     'name':'ravi',
+//     'roll':6,
+//     'xyz': nfun
+// }
+
+// function nfun(params) {
+//     console.log('hay ravi how are you')
+    
+// }
+// let getfun = person.xyz()
+
+
+
+// --------------------
+// const person = {
+//     'name':'ravi lakshakar',
+//     'roll':6,
+//     'xyz': nfun
+// }
+
+// function nfun(params) {
+//     console.log(`hay ${person.name} how are you`)
+    
+// }
+// let getfun = person.xyz()
+
+
+// --------------------
+// const person = {
+//     'name':'shiv lakshakar',
+//     'roll':6,
+//     'xyz': function nfun() {console.log(`hay ${this.name} how are you`)}
+// }
+
+// let getfun = person.xyz()
+
+// ---------------------
+
+// function nfun() {
+//     console.log(`hay ${this.name} how are you`)
+// }
+
+// const person = {
+//     'name':'shiv lakshakar',
+//     'roll':6,
+//     'xyz':  nfun
+// }
+// const person1 = {
+//     'name':'ravi lakshakar',
+//     'roll':6,
+//     'xyz':  nfun
+// }
+
+// person1.xyz()
+
+// -------------------------------
