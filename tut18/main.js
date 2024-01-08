95ec9e801f35c7d0354ddd95fe970373cd314fab
ravi lakshakar <ravilakshakar336@gmail.com>
2024-01-08 20:34:16 +0530

    update my code

---
+++ tut18/main.js
@@ -0,0 +1,14 @@
+// ------------------------ map function
+
+
+// let number = [1,2,3,4,5]
+
+// const multi =  (n)=>{
+//     return n*10
+
+// }
+
+// const newArray = number.map(multi)
+// console.log(newArray)
+
+
