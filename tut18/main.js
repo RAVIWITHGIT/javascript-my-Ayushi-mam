326a11ee1ff6bf4e0e0e43a1da3b1e2b6cb577bb
ravi lakshakar <ravilakshakar336@gmail.com>
2024-01-08 20:35:37 +0530

    modify my code

--- tut18/main.js
+++ tut18/main.js
@@ -12,3 +12,26 @@
 // console.log(newArray)
 
 
+// let student_details = {}
+
+// ------------ dynamic object , we can only change varaible value
+
+// const student_name = 'ravi'
+// const roll_no = 4
+// const status1 = true;
+
+// student_details = {
+//     name:student_name,
+//     roll_no:roll_no,
+//     status:status1
+// }
+
+// console.log(student_details)
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let text = fruits.constructor;
+// console.log(text('ravi','shiv'))
+
+
+
