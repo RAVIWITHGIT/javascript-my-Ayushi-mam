7f9a6a8daec400a398dc2711c8de4bd6e6146e21
RAVIWITHGIT <ravilakshakar336@gmail.com>
2024-03-02 13:11:31 +0530

    update code

--- app.js
+++ app.js
@@ -74,3 +74,104 @@ here we discuss about Metacharctor symbol in regular expression
 // }else{
 //     console.log("not match string")
 // }
+
+
+// ------------------------------******************************************************
+/*
+3.  .(dot symbol) --> match any one character  in word
+   I. .(dot symbol) ka upayog tab kiya jaata hai jab ham yah chaahate hain ki word ke beech mein ek specific character ho tabhee hee match ho ek se jyaada charactor ho to match na ho
+
+*/
+
+// -----> regex pattern mi /r.vi/ ke beech mein keval ek hee charactor hai
+
+// let regex = /r.vi/
+// let str = "ravi you know ravi"
+
+// let result = regex.exec(str)
+// console.log(result)
+
+
+// if(regex.test(str)){
+//     console.log(`the string ${str} match the expression ${regex.source}`)
+// }else{
+//     console.log("not match string")
+// }
+
+
+// ------> /r.vi/ ke beech mein ek se jyaada charactor ho to match nahin hoga
+// let regex = /r.vi/
+// let str = "rdjfjfjvi you know rttttyjavi"
+
+// let result = regex.exec(str)
+// console.log(result)
+
+
+// if(regex.test(str)){
+//     console.log(`the string ${str} match the expression ${regex.source}`)
+// }else{
+//     console.log("not match string")
+// }
+
+
+// ------------------------------------**********************************************************
+/*
+4. * (star symbol) matches any o or more characters
+*/
+// let regex = /r*vi/
+// let str = "rvi you know rttttyjavi"
+
+// let result = regex.exec(str)
+// console.log(result)
+
+
+// if(regex.test(str)){
+//     console.log(`the string ${str} match the expression ${regex.source}`)
+// }else{
+//     console.log("not match string")
+// }
+
+
+// ----------------------
+// let regex = /r*vi/
+// let str = "r44567vi you know rttttyjavi"
+
+// let result = regex.exec(str)
+// console.log(result)
+
+
+// if(regex.test(str)){
+//     console.log(`the string ${str} match the expression ${regex.source}`)
+// }else{
+//     console.log("not match string")
+// }
+
+
+//-----------------------
+// let regex = /ma*n/
+// let str = "maaaaan"
+
+// let result = regex.exec(str)
+// console.log(result)
+
+
+// if(regex.test(str)){
+//     console.log(`the string ${str} match the expression ${regex.source}`)
+// }else{
+//     console.log("not match string")
+// }
+
+
+// ----------------------
+// let regex = /ma*n/
+// let str = "main"
+
+// let result = regex.exec(str)
+// console.log(result)
+
+
+// if(regex.test(str)){
+//     console.log(`the string ${str} match the expression ${regex.source}`)
+// }else{
+//     console.log("not match string")
+// }
